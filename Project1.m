b75a8f81c51d614ecae806e43ee277c3611775f8
Alyssa Lawrence <crazy4drag0ns@yahoo.com>
2014-09-29 14:25:39 -0400

    Constant stimulation file and figures

--- Project1.m
+++ Project1.m
@@ -49,13 +49,13 @@ for t=0:.00025:.1
    if t<.005
        Vm(i) = Vrest;
    end
+   %find Vm during the step stimulation
    if t==.0005
        Ina = (m^3)*gna*h*(Vm(i-1)-Ena);
        Ik = (n^4)*gk*(Vm(i-1)-Ek);
        Il = gl*(Vm(i-1)-El);
        Iion = Ina+Ik+Il;
        Istim = .000005;
-       Itotal = Iion+Istim;
        Vm(i) = Vm(i-1)+((Istim/Cm)*.00025);
    end
    if t==.00075
@@ -66,7 +66,15 @@ for t=0:.00025:.1
        Istim = .000005;
        Vm(i) = Vm(i-1)+((Istim/Cm)*.00025);
    end
-   if t>.00075
+   if t==.001
+       Ina = (m^3)*gna*h*(Vm(i-1)-Ena);
+       Ik = (n^4)*gk*(Vm(i-1)-Ek);
+       Il = gl*(Vm(i-1)-El);
+       Iion = Ina+Ik+Il;
+       Istim = .000005;
+       Vm(i) = Vm(i-1)+((Istim/Cm)*.00025);
+   end
+   if t>.001
       Vm(i) = Vrest;
    end
    %calculate a and B values
@@ -84,6 +92,53 @@ for t=0:.00025:.1
    Gk(i) = gk*(n^4);
    Gna(i) = (m^3)*h*gna;
 end
+%set up the plots for step stimulation
+time = 0:.00025:.1;
+figure;
+plot(time,Gk);
+hold on;
+plot(time,Gna);
+hold off;
+figure;
+plot(time,Vm);
+
+%constant stimulation of 1uA for duration of simulation
+%create arrays for values to change with time hat will be plotted
+Vm = zeros(0,60);
+Gk = zeros(0,60);
+Gna = zeros(0,60);
+%for loop to collect change in values
+i = 0;
+for t=0:.00025:.1
+   i = i+1;
+   if t==0
+       Vm(i) = Vrest;
+   end
+   %find Vm during the step stimulation
+   if t>0
+       Ina = (m^3)*gna*h*(Vm(i-1)-Ena);
+       Ik = (n^4)*gk*(Vm(i-1)-Ek);
+       Il = gl*(Vm(i-1)-El);
+       Iion = Ina+Ik+Il;
+       Istim = .000001;
+       Vm(i) = Vm(i-1)+((Istim/Cm)*.00025);
+   end
+   %calculate a and B values
+   am = .1*((25-Vm(i))/(exp((25-Vm(i))/10)-1));
+   Bm = 4*exp(-Vm(i)/18);
+   an = .01*((10-Vm(i))/(exp((10-Vm(i))/10)-1));
+   Bn = .125*exp(-Vm(i)/80);
+   ah = .07*exp(-Vm(i)/20);
+   Bh = 1/(exp((30-Vm(i))/10)+1);
+   %calculate m,n,h values
+   n = an/(an+Bn);
+   m = am/(am+Bm);
+   h = ah/(ah+Bh);
+   %calculate current Gna and Gk
+   Gk(i) = gk*(n^4);
+   Gna(i) = (m^3)*h*gna;
+end
+%set up the plots for step stimulation
 time = 0:.00025:.1;
 figure;
 plot(time,Gk);

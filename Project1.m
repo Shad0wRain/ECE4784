f3a77fbf9044aa284e8624c5a747d66d69182374
Alyssa Lawrence <crazy4drag0ns@yahoo.com>
2014-09-29 14:05:22 -0400

    Step Stimulation file and figures

--- Project1.m
+++ Project1.m
@@ -26,8 +26,9 @@ h = ah/(ah+Bh);
 Ina = (m^3)*gna*h*(Vm-Ena);
 Ik = (n^4)*gk*(Vm-Ek);
 Il = gl*(Vm-El);
-Icm = 0;
 Iion = Ina+Ik+Il;
+Icm = 0;
+Itotal = Iion + Icm;
 %show plot of Gna/Gk
 Gk = gk*(n^4);
 Gna = (m^3)*h*gna;
@@ -36,5 +37,58 @@ plot([0,.1],[(Gna/Gk),(Gna/Gk)]);
 figure;
 plot([0,.1],[Vm,Vm]);
 
-%step pulse stimulation of 5uA for .5ms
-
+%step pulse stimulation of 5uA for .5ms starting at t = .5ms
+%create arrays for values to change with time hat will be plotted
+Vm = zeros(0,60);
+Gk = zeros(0,60);
+Gna = zeros(0,60);
+%for loop to collect change in values
+i = 0;
+for t=0:.00025:.1
+   i = i+1;
+   if t<.005
+       Vm(i) = Vrest;
+   end
+   if t==.0005
+       Ina = (m^3)*gna*h*(Vm(i-1)-Ena);
+       Ik = (n^4)*gk*(Vm(i-1)-Ek);
+       Il = gl*(Vm(i-1)-El);
+       Iion = Ina+Ik+Il;
+       Istim = .000005;
+       Itotal = Iion+Istim;
+       Vm(i) = Vm(i-1)+((Istim/Cm)*.00025);
+   end
+   if t==.00075
+       Ina = (m^3)*gna*h*(Vm(i-1)-Ena);
+       Ik = (n^4)*gk*(Vm(i-1)-Ek);
+       Il = gl*(Vm(i-1)-El);
+       Iion = Ina+Ik+Il;
+       Istim = .000005;
+       Vm(i) = Vm(i-1)+((Istim/Cm)*.00025);
+   end
+   if t>.00075
+      Vm(i) = Vrest;
+   end
+   %calculate a and B values
+   am = .1*((25-Vm(i))/(exp((25-Vm(i))/10)-1));
+   Bm = 4*exp(-Vm(i)/18);
+   an = .01*((10-Vm(i))/(exp((10-Vm(i))/10)-1));
+   Bn = .125*exp(-Vm(i)/80);
+   ah = .07*exp(-Vm(i)/20);
+   Bh = 1/(exp((30-Vm(i))/10)+1);
+   %calculate m,n,h values
+   n = an/(an+Bn);
+   m = am/(am+Bm);
+   h = ah/(ah+Bh);
+   %calculate current Gna and Gk
+   Gk(i) = gk*(n^4);
+   Gna(i) = (m^3)*h*gna;
+end
+time = 0:.00025:.1;
+figure;
+plot(time,Gk);
+hold on;
+plot(time,Gna);
+hold off;
+figure;
+plot(time,Vm);
